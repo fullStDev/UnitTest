40573a0cba446eda90438798eebd6749f951c715
Вадим Ермаков <vadimka_ermakov_3@mail.ru>
2021-10-09 14:47:09 +0400

    Добавьте файлы проекта.

---
+++ ErmakovISP_31ClassLib/ErmakovISP_31Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ErmakovISP_31ClassLib
+{
+    public class ErmakovISP_31Class
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ErmakovISP_31ClassLibTests/ErmakovISP_31ClassTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ErmakovISP_31ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErmakovISP_31ClassLib.Tests
+{
+    [TestClass()]
+    public class ErmakovISP_31ClassTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            int x = 10;
+            int y = 5;
+            int expected = 15;
+
+            ErmakovISP_31Class Er = new ErmakovISP_31Class();
+            int actual = Er.Sum(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
